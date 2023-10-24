30919c4b4592b418361d0504b2da3e34ad798d5a
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-10-24 13:29:05 +0200

    handle non ascii stuff better

--- scripts/printer.py
+++ scripts/printer.py
@@ -1,6 +1,7 @@
 from mastodon import Mastodon
 import os
-import time
+from unidecode import unidecode
+import emoji
 from io import StringIO
 from html.parser import HTMLParser
 from escpos.printer import Usb
@@ -14,15 +15,20 @@ class MLStripper(HTMLParser):
         self.strict = False
         self.convert_charrefs= True
         self.text = StringIO()
+
+    def handle_endtag(self, tag):
+        if tag in ["br", "p"]:
+            self.text.write("\n")
+
     def handle_data(self, d):
         self.text.write(d)
     def get_data(self):
         return self.text.getvalue()
 
-def strip_tags(html):
+def asciify(html):
     s = MLStripper()
     s.feed(html)
-    return s.get_data()
+    return unidecode(emoji.demojize(s.get_data()))
 
 #   Set up Mastodon
 mastodon = Mastodon(
@@ -32,13 +38,13 @@ mastodon = Mastodon(
 for notification in mastodon.notifications(types=["mention"]):
     status = notification["status"]
     printer.text("%s sent you this message at %s:\n" % (status["account"]["acct"], status["created_at"]))
-    printer.text(strip_tags(status["content"])+"\n")
+    printer.text(asciify(status["content"])+"\n")
     replied_to_id = status["in_reply_to_id"]
     if replied_to_id:
         replied_to = mastodon.status(replied_to_id)
         printer.text("In reply to this toot from %s:\n" % replied_to["account"]["acct"])
         printer.text(replied_to["url"]+"\n")
-        printer.text(strip_tags(replied_to["content"])+"\n")
+        printer.text(asciify(replied_to["content"])+"\n")
     printer.text("--------------\n\n\n\n")
     mastodon.notifications_dismiss(notification["id"])
 printer.close()
