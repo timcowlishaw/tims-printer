ef85df5fb7fbcdc9d4194af7489797e162a69bb9
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-10-24 12:45:09 +0200

    pi installation gubbins

--- pi-config/install.sh
+++ pi-config/install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
-sudo pip install virtualenv
-sudo apt-get install python3-usb1 vim
-git clone git@github.com:timcowlishaw/tims-printer.git
-cd tims-printer
+sudo apt-get install python3-usb1 python3-virtualenv vim
 virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
 sudo cp ./pi-config/50-xprinter.rules /etc/udev/rules.d/
+sudo cp ./pi-config/printer.cron /etc/cron.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger
